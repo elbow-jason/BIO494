a620d9c6c0703c8784cd5054d27a861ba6304b83
Jason Goldberger <jlgoldb2@asu.edu>
2015-03-19 14:46:57 -0700

    added script

--- midterm/my_script.sh
+++ midterm/my_script.sh
@@ -8,4 +8,5 @@ NAME=`echo "$FILENAME" | cut -d'.' -f1`
 EXTENSION=`echo "$FILENAME" | cut -d'.' -f2`
 
 echo "splitting fasta away from gff"
+
 cat $FILENAME | sed -n -e '/FASTA/,$p' | tail -n +2 | head
